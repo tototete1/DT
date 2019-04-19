0d7284ba98b67423b17545d894dddb055c942d6f
tototete1 <45425004+tototete1@users.noreply.github.com>
2019-04-19 12:30:35 +0700

    Add files via upload

--- HVSTR.cpp
+++ HVSTR.cpp
@@ -18,25 +18,39 @@ long long int tinh(int r)
     }
     return s;
 }
+long long int tinh2(int r)
+{
+    int i,s=1;
+    for (i=0; i<=k; i++)
+    {
+        if (i!=r)
+            s=s*gt[b[i]];
+        else
+            s=s*gt[b[i]-1];
+    }
+    return s;
+}
 int main()
 {
     ofstream fo ("HVSTR.out");
     ifstream fi ("HVSTR.inp");
-    int i,j;
+    int i,j,t,p;
     fi >>s;
     fi >>m;
     memcpy(c,s,sizeof(s));
     memcpy(w,s,sizeof(s));
     n=strlen(s);
+    t=strlen(w);
+    p=strlen(c);
     sort(s,s+n);
-    sort(w,w+n);
+    sort(w,w+t);
     gt[0]=1;
     for (i=1; i<=20; i++)
         gt[i]=gt[i-1]*i;
     //tim vi tri
     {
         x=0;
-        k=0,d[0]=1;
+        k=0,b[0]=1;
         for (i=1; i<n; i++)
         {
             if (w[i]==w[i-1])
@@ -47,19 +61,28 @@ int main()
                 b[k]=1,w[k]=w[i];
             }
         }
-        for (i=0;i<n;i++)
+        //for (i=0;i<=k;i++) cout <<b[i]<<" ";
+        for (i=0; i<p; i++)
         {
-            for (j=0;j<=k;j++)
+            j=0;
+            for (j=0; j<=k; j++)
             {
-                if (w[j]<c[i])
+                if (b[j]>0)
                 {
-                    M=M+gt[n-i]/tinh(j);
+                    if (w[j]<c[i])
+                    {
+                        //cout <<w[j]<<" ";
+                        M=M+gt[p-i-1]/tinh2(j);
+                        //cout <<i<<" "<<M<<" ";
+                    }
+                    else
+                        break;
                 }
-                else break;
             }
+            //cout <<endl;
             b[j]--;
         }
-        cout <<M+1;
+        fo <<M+1<<endl;
     }
     //tim cau hinh
     {
