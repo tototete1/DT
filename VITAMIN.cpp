7e31904550d4d5e88ffc3a52df551ce9205404eb
tototete1 <45425004+tototete1@users.noreply.github.com>
2019-06-18 08:53:01 +0700

    Update VITAMIN.cpp

--- VITAMIN.cpp
+++ VITAMIN.cpp
@@ -1,48 +1,71 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 int n,x,i,j,u,c;
 long long int s[101],f[101][1000001];
 int main()
 {
-    cin >>n>>x;
-    for (i=1;i<=n;i++){
+    ifstream fi ("VITAMIN.inp");
+    fi >>n>>x;
+    for (i=1; i<=n; i++)
+    {
         cin >>c;
         s[i]=s[i-1]+c;
     }
     u=1;
-    for (i=1;i<=x;i++)
+    for (i=1; i<=x; i++)
     {
-        if (i%s[1]==0) f[1][i]=u,u+=2;
+        if (i%s[1]==0)
+            f[1][i]=u,u+=2;
     }
-    for (i=2;i<=n;i++)
+    u=c;
+    int w=s[n]+u;
+    for (i=2; i<=n; i++)
     {
-        for (j=1;j<=x;j++)
+        for (j=1; j<=x; j++)
         {
-            if (j<s[i]) f[i][j]=f[i-1][j];
-            if (j==s[i]) f[i][j]=i;
-            if (j>s[i])
+            if (i!=n)
             {
-                if (f[i-1][j-s[i]]!=0){
-
-                    f[i][j]=f[i-1][j-s[i]]+i+1;
-                    if (i==2 && j==12) cout <<"AAA";
+                if (j<s[i])
+                    f[i][j]=f[i-1][j];
+                if (j==s[i])
+                    f[i][j]=i;
+                if (j>s[i])
+                {
+                    if (j%s[i]==0) f[i][j]=f[i][j-s[i]]+i+1;
+                    else if (f[i-1][j-s[i]]!=0) f[i][j]=f[i-1][j-s[i]]+i+1;
+                }
+            }
+            if (i==n){
+                if (j<s[i]) f[i][j]=f[i-1][j];
+                if (j==s[i]) f[i][j]=i;
+                if (j>s[i]){
+                    if (j%w==0) f[i][j]=f[i][j-u]+1;
+                    else{
+                        if (f[i-1][j-s[i]]!=0) f[i][j]=f[i-1][j-s[i]]+i+1;
+                    }
                 }
             }
         }
     }
-    for (i=1;i<=n;i++)
+    ofstream fo ("VITAMIN.out");
+    /*for (i=1; i<=n; i++)
     {
-        for (j=1;j<=x;j++) cout <<f[i][j]<<" ";
+        for (j=1; j<=x; j++)
+            cout <<f[i][j]<<" ";
         cout <<endl;
-    }
+    }*/
     long long int res=1e18;
-    for (i=1;i<=n;i++){
-        if (f[i][x]!=0){
+    for (i=1; i<=n; i++)
+    {
+        if (f[i][x]!=0)
+        {
             res=min(res,f[i][x]);
         }
     }
-    if (res==1e18) cout <<-1;
-    else cout <<res;
+    if (res==1e18)
+        fo <<-1;
+    else
+        fo <<res;
     return 0;
 }
